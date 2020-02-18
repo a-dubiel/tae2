16e8fcc1a4bd5ad000674d6b5570aac114b532f7
Andrzej Dubiel <andrzej@gothrive.io>
2020-02-18 14:33:32 +0100

    og fix

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -36,10 +36,7 @@ const TemplateWrapper = ({ children }) => {
         <meta property="og:type" content="business.business" />
         <meta property="og:title" content={title} />
         <meta property="og:url" content="/" />
-        <meta
-          property="og:image"
-          content={`${withPrefix("/")}img/og-image.jpg`}
-        />
+        <meta property="og:image" content={`${withPrefix("/")}img/og.jpg`} />
       </Helmet>
       <main className="main">{children}</main>
     </React.Fragment>
